dc338adb1ccc780a4fcf7172740b3e8113c178da
hfaran <hamzafaran@outlook.com>
2016-03-23 01:08:36 -0700

    Added some other not as good crossover methods

--- idgaf/examples/tsp/__init__.py
+++ idgaf/examples/tsp/__init__.py
@@ -39,6 +39,8 @@ class TSPState(State):
     """
     def __init__(self, route, cities, distance_matrix):
         self.route = route
+        # self.start_node = start_node
+        # assert self.route[0] == self.start_node
         self.cities = cities
         self.distance_matrix = distance_matrix
 
@@ -67,7 +69,123 @@ class TSPState(State):
                     child_ptr = endpos
         # Just in case the algorithm above is broken
         assert len(set(child)) == route_len
-        return TSPState(child, self.cities, self.distance_matrix)
+        #return TSPState(child, self.cities, self.distance_matrix)
+        self.route = child
+        return self
+
+    def crossover2(self, other):
+        d = {}
+        my_route = self.route[:]
+        my_start = self.route[0]
+        other_index = other.route.index(my_start)
+        other_route = other.route[other_index:]+other.route[:other_index]
+
+        for i, node in enumerate(my_route[:-1]):
+            d[node] = {"self": my_route[i+1]}
+        d[my_route[-1]] = {"self": my_start}
+        for i, node in enumerate(other_route[:-1]):
+            try:
+                d[node]["other"] = other_route[i+1]
+            except KeyError:
+                d[node] = {"other": other_route[i+1]}
+        d[other_route[-1]]["other"] = my_start
+
+        # child_connections = {}
+        # ssroute = set(my_route)
+        # for i, node in enumerate(sorted(other_route[:-1])):
+        #     ccv = set(child_connections.values())
+        #     candidates = d[node].values()
+        #     random.shuffle(candidates)
+        #     for c in candidates:
+        #         if c not in ccv:
+        #             if node == c or c == my_start or child_connections.get(
+        #                     node) == c or (c == other_route[-1] and (i!=len(
+        #                 my_route)-2)) or child_connections.get(c) in \
+        #                     child_connections:
+        #                 continue
+        #             child_connections[node] = c
+        #             print(1)
+        #             break
+        #     else:
+        #         child_connections[node] = random.choice(list(
+        #             ssroute - ccv - {my_start}))
+        #         print(2)
+        #     print(child_connections)
+        # assert len(child_connections) == len(my_route)-1
+
+        # child = [my_start]
+        # node = my_start
+        # while node:
+        #     node = child_connections.get(node)
+        #     child.append(node)
+        # assert child[-1] is None
+        # child = child[:-1]
+        # assert len(child) == len(my_route)
+        # assert len(set(child)) == len(child)
+        # # return TSPState(child, self.cities, self.distance_matrix)
+        # self.route = child
+        # return self
+
+        # TODO this might work well but might have a bug; double-check later
+        new_route = [my_start]
+        #current = my_start
+        destinations = my_route[1:]
+        while destinations:
+            current = new_route[-1]
+            candidates = d[current].values()
+            random.shuffle(candidates)
+            for c in candidates:
+                if c not in destinations:
+                    continue
+            else:
+                c = random.choice(destinations)
+            new_route.append(c)
+            destinations.remove(c)
+        assert len(new_route) == len(my_route)
+        assert len(set(new_route)) == len(new_route)
+        #self.route = new_route
+        #return self
+        return TSPState(new_route, self.cities, self.distance_matrix)
+
+
+    def crossover3(self, other):
+        """
+
+        :type other: TSPState
+        :param other:
+        :return:
+        """
+        route_len = len(self.route)
+        cities_to_replace = set(random.sample(
+            self.route,
+            random.randint(int(round(route_len/4.0)), int(round(
+                route_len*3/4.0))-1)
+        ))
+        other_route = [node for node in other.route
+                       if node in cities_to_replace]
+        # child = self.route[:]
+        # for i, node in enumerate(child[:]):
+        #     if not other_route:
+        #         break
+        #     if node == other_route[0]:
+        #         child[i] = other_route.pop(0)
+
+        # _my_route = self.route[:]
+        # _other_route = other.route[:]
+        child = []
+        nodes = self.route[:]
+        while nodes:
+            node = nodes[0]
+            if node in cities_to_replace:
+                other_node = other_route.pop(0)
+                child.append(other_node)
+                nodes.remove(other_node)
+            else:
+                child.append(nodes.pop(0))
+
+        #return TSPState(child, self.cities, self.distance_matrix)
+        self.route = child
+        return self
 
     def mutate(self):
         state = self.route
