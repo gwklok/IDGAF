79a4ee638c18ff425df76e7d8e3b86fb10e93038
hfaran <hamzafaran@outlook.com>
2016-03-23 11:11:10 +0100

    Merge

--- tests/performance_bench.py
+++ tests/performance_bench.py
@@ -4,4 +4,4 @@ from idgaf.examples.tsp.cities import cities_7, cities_120
 
 # tsp_auto_example(minutes=3, cities=cities_120)
 tsp_parallel_test(cities=cities_120, metagenerations=None,
-                  target_fitness=-300000)
+                  target_fitness=-60000)
